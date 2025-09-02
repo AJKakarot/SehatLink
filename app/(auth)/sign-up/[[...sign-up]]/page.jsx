3c2275c0e388bbf1d891856eb26226841294362a
ajeet gupta <140450935+AJKakarot@users.noreply.github.com>
2025-09-03 00:00:36 +0530

    final submit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-start min-h-screen pt-20 pb-10">
+      <SignUp />
+    </div>
+  );
+}
