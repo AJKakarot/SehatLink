cf8e0a1f66a8a7b6208014190d7e23d7cc130dcb
ajeet gupta <140450935+AJKakarot@users.noreply.github.com>
2025-09-03 01:01:47 +0530

    final

--- middleware.js
+++ middleware.js
@@ -20,9 +20,10 @@ export default clerkMiddleware((auth, req) => {
   return NextResponse.next();
 });
 
-// Ensure matcher works properly with Edge runtime
+// 👇 export runtime separately
+export const runtime = "nodejs";
+
 export const config = {
-  runtime: "nodejs",
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
