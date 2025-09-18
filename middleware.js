df5d0b9da351583aaeff7e1e4dacb9cc2c232fe0
ajeet gupta <140450935+AJKakarot@users.noreply.github.com>
2025-09-19 03:21:02 +0530

    deploy

--- middleware.js
+++ middleware.js
@@ -1,10 +1,6 @@
-// Force Node runtime instead of Edge
-export const runtime = "nodejs";
-
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// List of routes that require authentication
 const isProtectedRoute = createRouteMatcher([
   "/doctors(.*)",
   "/onboarding(.*)",
@@ -18,9 +14,8 @@ export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
-    // Use redirectToSignIn from auth
     const { redirectToSignIn } = await auth();
-    return redirectToSignIn({ returnBackUrl: req.url });
+    return redirectToSignIn();
   }
 
   return NextResponse.next();
@@ -28,7 +23,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
     "/(api|trpc)(.*)",
